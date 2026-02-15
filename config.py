956d6e0a29b6d6e8d2b8599c158e6f2d9149309d
iammohit-kenshin <iamnotmohit.1@gmail.com>
2026-02-15 13:34:02 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+API_ID = int(os.environ.get("API_ID"))
+API_HASH = os.environ.get("API_HASH")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+
+ADMIN_ID = int(os.environ.get("ADMIN_ID"))
+STORAGE_GROUP_ID = int(os.environ.get("STORAGE_GROUP_ID"))
+
+DOWNLOAD_PATH = "downloads/"
